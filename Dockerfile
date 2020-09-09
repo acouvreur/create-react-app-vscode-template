4660f64c2e2ca22bf9bd39498cbc15b459e8e847
Alexis Couvreur <alexiscouvreur.pro@gmail.com>
2020-09-10 00:16:38 +0200

    Added flow

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,20 @@
-FROM nginx:alpine
+FROM node:12-alpine as builder
 
-COPY ./build /usr/share/nginx/html
+WORKDIR /app
+
+# install app dependencies
+COPY package.json ./
+COPY package-lock.json ./
+
+RUN npm ci --silent
+
+COPY . ./
+
+RUN npm run build
+
+FROM nginx:stable-alpine
+
+COPY --from=builder /app/build /usr/share/nginx/html
+EXPOSE 80
+
+CMD ["nginx", "-g", "daemon off;"]
--- src/App.spec.js
+++ src/App.spec.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import { render } from '@testing-library/react'
-import App from './App'
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-describe('a', () => {
-  it('renders learn react link', () => {
-    expect.assertions(1)
-    const { getByText } = render(<App />)
-    const linkElement = getByText(/learn react/i)
-    expect(linkElement).toBeInTheDocument()
-  })
-})
+describe("a", () => {
+  it("renders learn react link", () => {
+    expect.assertions(1);
+    const { getByText } = render(<App />);
+    const linkElement = getByText(/learn react!/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+});
