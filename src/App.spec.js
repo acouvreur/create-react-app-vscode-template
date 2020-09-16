d1d7a9fd7eae052ad1be9a149e45a7633c279255
Alexis Couvreur <alexiscouvreur.pro@gmail.com>
2020-09-16 20:57:18 +0200

    Update deps

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,9 +1,9 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import App from "./App";
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
 
-describe("a", () => {
-  it("renders learn react link", () => {
+describe('a', () => {
+  it('renders learn react link', () => {
     expect.assertions(1);
     const { getByText } = render(<App />);
     const linkElement = getByText(/learn react!/i);
